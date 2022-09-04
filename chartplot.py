f186045485366ceb4da37cd6a391b706ad90ae10
Daniel Ince <incynerated@gmail.com>
2022-09-05 00:19:58 +0100

    zzz

--- chartplot.py
+++ chartplot.py
@@ -1,3 +1,12 @@
-print("hello world")
-print("test")
-print("again")
+import ccxt
+import pandas as pd 
+
+binance = ccxt.binance()
+binancemarkets = binance.load_markets()
+binance_eth = binance.markets['ETH/USDT']
+print(binance_eth[2])
+
+ftx = ccxt.ftx()
+ftxmarkets = ftx.load_markets()
+
+#print(ccxt.exchanges)
